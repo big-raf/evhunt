5bcf7f3ff652107f2874c834d1353a58c6fd1410
meng <hanmengue@gmail.com>
2018-05-18 02:09:43 +0200

    [EVHUNT]frontend manager process service to get offers

---
+++ evhunt/src/main/webapp/app/layouts/manager-follow-up/PosteService.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class PosteService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    getAllByManager(): Observable<any> {
+        return this.http.get('//localhost:8080//findAllPostes');
+    }
+
+    getAllCandidatesByPoste(id): Observable<any> {
+        return this.http.get('//localhost:8080//findCandidatureByPosteId'+"/?id="+id);
+    }
+}
